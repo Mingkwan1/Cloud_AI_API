d0dda27f0fe258927192918ac95b9c728cbf1a2b
Mingkwan Rattanasiwamok <mingkwan.ratta@gmail.com>
2025-04-24 20:04:41 +0900

    minor issue

--- app/handler.py
+++ app/handler.py
@@ -17,18 +17,21 @@ def handler(event):
     print(">>> handler.py called ✅")
     # Extract input data
     print(f"Worker Start")
-    input = event['input']
-    
-    prompt = input.get('prompt')  
-    seconds = input.get('seconds', 0)  
+    try:
+        input = event['input']
+        
+        prompt = input.get('prompt')  
+        seconds = input.get('seconds', 0)  
 
-    print(f"Received prompt: {prompt}")
-    print(f"Sleeping for {seconds} seconds...")
-    
-    # You can replace this sleep call with your Python function to generate images, text, or run any machine learning workload
-    result = get_answer(prompt)
-    return result 
-
+        print(f"Received prompt: {prompt}")
+        print(f"Sleeping for {seconds} seconds...")
+        
+        # You can replace this sleep call with your Python function to generate images, text, or run any machine learning workload
+        result = get_answer(prompt)
+        return result 
+    except Exception as e:
+        print(f"Error in handler: {str(e)}")
+        raise e  # Ensure the error gets captured
 # Start the Serverless function when the script is run
 if __name__ == '__main__':
     print("🔧 Starting serverless handler...")
